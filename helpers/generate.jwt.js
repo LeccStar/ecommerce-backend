46ab4cce45fcf034b10f53456003166a2c04caac
Luis <le.cervantes.96@gmail.com>
2022-12-15 14:28:07 -0600

    removes exiraion at token service

--- helpers/generate.jwt.js
+++ helpers/generate.jwt.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const generateJWT = (uid = "") => {
     return new Promise((resolve, reject) => {
         const payload = { uid };
-        jwt.sign(payload, process.env.SECRET_JWT, { expiresIn: '8h' }, (err, token) => {
+        jwt.sign(payload, process.env.SECRET_JWT/* , { expiresIn: '8h' } */, (err, token) => {
             if (err) {
                 console.log(err);
                 reject("token not generated")
